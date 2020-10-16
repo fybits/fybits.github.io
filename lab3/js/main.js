89c366a16c3531693b1d8ae2d47184de31bea882
TryThisCookie <nevzgodov.anton@gmail.com>
2020-10-16 12:27:06 +0300

    Implemented tools interface

--- lab3/js/main.js
+++ lab3/js/main.js
@@ -99,69 +99,88 @@ async function fillBucket(x, y, c) {
 }
 
 let mousePos = { x: 0, y: 0 };
-let lastPos = { x: 0, y: 0 };
-
-let isDrawing = false;
+let lastMousePos = { x: 0, y: 0 };
 
 let toolIndex = 0;
 let colorIndex = 0;
 let colorName = '';
 
+const pencilTool = {
+  buttonId: "#pencil-button",
+  isDrawing: false,
+  onDown() {
+    this.isDrawing = true;
+    lastMousePos.x = mousePos.x;
+    lastMousePos.y = mousePos.y;
+  },
+  onUpdate() {
+    if (this.isDrawing) {
+      drawLine(mousePos.x, mousePos.y, lastMousePos.x, lastMousePos.y, color(colorName))
+      lastMousePos.x = mousePos.x;
+      lastMousePos.y = mousePos.y;
+    }
+  },
+  onUp() {
+    this.isDrawing = false;
+  },
+};
+
+const bucketTool = {
+  buttonId: "#bucket-button",
+  onDown() {
+    fillBucket(mousePos.x, mousePos.y, color(colorName))
+  },
+  onUp() {},
+  onUpdate() {},
+}
+
+const tools = [ pencilTool, bucketTool ];
+
 function setup () {
   createCanvas(window.innerWidth, window.innerHeight, 'WEBGL');
+
   const canvas = document.querySelector('#defaultCanvas0');
   const rect = canvas.getBoundingClientRect();
+
   document.body.addEventListener('mousemove', (event) => {
-    mousePos = {
-      x: event.clientX - rect.x,
-      y: event.clientY - rect.y,
-    }
-  })
-  canvas.addEventListener('mousedown', () => {
-    isDrawing = true;
-    if (toolIndex === 1) fillBucket(mousePos.x, mousePos.y, color(colorName))
-    last = mousePos;
+    mousePos.x = event.clientX - rect.x;
+    mousePos.y = event.clientY - rect.y;
   });
-  document.body.addEventListener('mouseup', () => {
-    isDrawing = false;
-  })
+
+  canvas.addEventListener('mousedown', () => tools[toolIndex].onDown());
+  document.body.addEventListener('mouseup', () => tools[toolIndex].onUp());
+
   loadPixels();
   clearScreen();
   updatePixels();
-  document.querySelector('#save-button').addEventListener('click', () => {
-    save('myCanvas.jpg');
-  })
-  const toolsButtons = [
-    document.querySelector('#pencil-button'),
-    document.querySelector('#bucket-button'),
-  ]
 
+  const toolsButtons = tools.map(tool => document.querySelector(tool.buttonId));
+  console.log(tools);
+  console.log(toolsButtons);
+  toolsButtons[toolIndex].classList.add('active');
   toolsButtons.forEach((button, idx) => button.addEventListener('click', () => {
     toolsButtons[toolIndex].classList.remove('active');
     toolIndex = idx;
     button.classList.add('active');
   }));
-  toolsButtons[toolIndex].classList.add('active');
 
   const colorButtons = document.querySelectorAll('.toolbar-item.color-circle');
+  colorButtons[colorIndex].classList.add('active');
+  colorName = colorButtons[colorIndex].getAttribute('data-color');
   colorButtons.forEach((button, idx) => button.addEventListener('click', () => {
     colorButtons[colorIndex].classList.remove('active');
     colorIndex = idx;
     colorName = button.getAttribute('data-color');
     button.classList.add('active');
   }));
-  colorButtons[colorIndex].classList.add('active');
-  colorName = colorButtons[colorIndex].getAttribute('data-color');
+
+  const saveButton = document.querySelector('#save-button');
+  saveButton.addEventListener('click', () => save('myCanvas.jpg'));
 }
-
-
 
 function draw() {
   loadPixels();
-  if (isDrawing && !toolIndex) {
-    drawLine(mousePos.x, mousePos.y, last.x, last.y, color(colorName))
-    last = mousePos;
-  }
+  tools[toolIndex].onUpdate();
   updatePixels();
 }
 
